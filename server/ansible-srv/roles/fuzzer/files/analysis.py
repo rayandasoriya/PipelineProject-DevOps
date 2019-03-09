234d51e381ef40e764fdb2e302823f399a57021b
Rayan Dasoriya <dasoriyarayan@gmail.com>
2019-03-09 16:26:31 -0500

    Test prioritazation fix

--- server/ansible-srv/roles/fuzzer/files/analysis.py
+++ server/ansible-srv/roles/fuzzer/files/analysis.py
@@ -5,24 +5,25 @@ from operator import itemgetter
 
 #files = os.listdir("/Users/rayandasoriya/Desktop/Projects/devops/iTrust-fuzzer/BuildResults/1/surefire-reports/")
 files = os.listdir("/home/vagrant/BuildResults/1/surefire-reports/")
+
 file = []
 for i in files:
     if ".xml" in i:
         file.append(i)
+        count+=1
 count = 0
 failed = 0
 passed = 0
+errored = 0
 new_arr = []
 info = []
 
-#f_len = os.listdir("/Users/rayandasoriya/Desktop/Projects/devops/iTrust-fuzzer/BuildResults/")
 f_len = os.listdir("/home/vagrant/BuildResults/")
-print(f_len)
 
 for f in file:
     for i in range(1,len(f_len)):
-        #f_name = '/Users/rayandasoriya/Desktop/Projects/devops/iTrust-fuzzer/BuildResults/'+str(i)+'/surefire-reports/'+f
         f_name = '/home/vagrant/BuildResults/'+str(i)+'/surefire-reports/'+f
+        #f_name = '/Users/rayandasoriya/Desktop/Projects/devops/iTrust-fuzzer/BuildResults/'+str(i)+'/surefire-reports/'+f
         file = open(f_name, 'r') 
         content = file.read()
         a = xmltodict.parse(content)
@@ -34,6 +35,11 @@ for f in file:
                     if "failure" in a["testsuite"]["testcase"].keys() :
                         info.append([i, a["testsuite"]["testcase"]["@name"], a["testsuite"]["testcase"]["@time"], "failed"])
                         failed += 1
+                    
+                    elif "error" in a["testsuite"]["testcase"].keys() :
+                        info.append([i, a["testsuite"]["testcase"]["@name"], a["testsuite"]["testcase"]["@time"], "errored"])
+                        errored += 1
+
                     else:
                         info.append([i, a["testsuite"]["testcase"]["@name"], a["testsuite"]["testcase"]["@time"], "passed"])
                         passed += 1
@@ -42,6 +48,9 @@ for f in file:
                 if "failure" in a["testsuite"]["testcase"][j].keys():
                     info.append([i, a["testsuite"]["testcase"][j]["@name"], a["testsuite"]["testcase"][j]["@time"], "failed"])
                     failed +=1
+                elif "error" in a["testsuite"]["testcase"][j].keys():
+                    info.append([i, a["testsuite"]["testcase"][j]["@name"], a["testsuite"]["testcase"][j]["@time"], "errored"])
+                    errored +=1
                 else:
                     info.append([i, a["testsuite"]["testcase"][j]["@name"], a["testsuite"]["testcase"][j]["@time"], "passed"])
                     passed += 1
@@ -53,29 +62,31 @@ test = set()
 for m in info:
     test.add(m[1])
 test = tuple(test)
-count_ff = 0
-count_pp = 0
 ttime = 0
 res = []
 for i in test:
     count_ff = 0
     count_pp = 0
+    count_ee = 0
     time = 0
     for j in info:
         if j[1] == i:
             time+=float(j[2])
             if j[3] == 'failed':
                 count_ff += 1
+            elif j[3] == 'errored':
+                count_ee += 1
             else: 
                 count_pp += 1
-    total = count_ff+count_pp
+    total = count_ff+count_pp+count_ee
     avg_time = round(time/total,2)
     ttime+=time
-    res.append([i,count_pp,count_ff,(-1*avg_time)])
+    res.append([i,count_pp,count_ff,count_ee,(-1*avg_time)])
 
-res = sorted(res, key= itemgetter(2,3), reverse = True)
+res = sorted(res, key= itemgetter(2,4), reverse = True)
 
 for m in res:
-    print('TestName: {}, Failed: {}, AvgTime: {} '.format( m[0],m[2],(-1*m[3])))
+    print(m[0])
+    print('Passed: {}, Failed: {}, Errored: {}, AvgTime: {} '.format(m[1],m[2],m[3],(-1*m[4])))
 
-print('Total Passed: {}, Total Failed: {}, Total Time: {}'.format(passed,failed,(round(ttime,2))))
+print('Total Passed: {}, Total Failed: {}, Total Errors: {}, Total Time: {}'.format(passed,failed,errored,(round(ttime,2))))
