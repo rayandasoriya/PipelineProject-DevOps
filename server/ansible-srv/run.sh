2a0e4020bf64d599124b640dbf2ee21eb97d2ce6
System Administrator <root@Jubeens-MacBook-Pro.local>
2019-04-10 02:16:14 -0400

    cleaning up code

--- server/ansible-srv/run.sh
+++ server/ansible-srv/run.sh
@@ -1,2 +1,5 @@
-sudo ansible-playbook setUpLocal.yml
-sudo ansible-playbook -i inventory playbook.yml
+sudo ansible-playbook setUpLocal.yml --vault-id password.txt
+sleep 5
+sudo ansible all -m ping -i inventory
+sleep 5
+sudo ansible-playbook -i inventory playbook.yml --vault-id password.txt
