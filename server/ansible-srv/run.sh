eeeca9071cdc87e6bb156933ec072fe09a40510d
Yo <jnshah2@ncsu.edu>
2019-04-22 10:12:44 -0400

    Removed Credentials

--- server/ansible-srv/run.sh
+++ server/ansible-srv/run.sh
@@ -1,8 +1,12 @@
+echo "Setting up Localhost"
 sudo ansible-playbook setUpLocal.yml --vault-id password.txt
 cd ~/AWS/
-node aws.js
+echo "Deploying AWS instance"
+sudo node aws.js
 sleep 10
 cd /ansible-srv/
+echo "Testing ping"
 sudo ansible all -m ping -i inventory
+echo "Running playbook.yml"
 sleep 10
 sudo ansible-playbook -i inventory playbook.yml --vault-id password.txt
